2d2198855be9a62c5d9c3398e2fc86117fba4d13
TaiStudent <xTaig4@gmail.com>
2024-12-13 10:42:03 +0100

    improved design, added hovered effect

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,12 +1,27 @@
+import './Project.css';
 import React from 'react';
+import { useState } from 'react';
 
 function Project(props) {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true)
+    }
+
+    const handleMouseLeave = () => {
+        setIsHovered(false)
+     }
+
     return (
-        <div className="project">
+        <div
+            className={`container ${isHovered ? "hovered" : ""}`}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}>
             <img className="project-image" src={props.imageUrl} alt={props.title} height="400" />
             <div className="project-info">
                 <h3>{props.title}</h3>
-                <h6>{ props.projectUrl}</h6>
+                {/* <h6>{ props.projectUrl}</h6> */}
             </div>
 
         </div>
